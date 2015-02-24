077cfbdd95907367dbf63841cb29b139a9ea1428
Gernot Gradwohl <grnt.grdwhl@gmail.com>
2015-02-24 22:18:42 +0100

    NEW: Basic functionality. Send messages and receive messages
    No room creation, no login, ...

---
+++ room.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"chat/tracer"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+type room struct {
+	forward chan []byte
+	join    chan *client
+	leave   chan *client
+	clients map[*client]bool
+
+	tracer tracer.Tracer
+}
+
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+	}
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+			// r.tracer.Trace("New Client")
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+			// r.tracer.Trace("Client left")
+		case msg := <-r.forward:
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// r.tracer.Trace("Message send %s", msg)
+				default:
+					delete(r.clients, client)
+					close(client.send)
+					// r.tracer.Trace("Faild to deliver Message. Client closed")
+				}
+			}
+		}
+	}
+}
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: messageBufferSize}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP: ", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	fmt.Printf("Socket verbunden %+v", client)
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
+}
