73a2ced12a339dba5c3b99313a4ded7a957f2d4f
choallin <grnt.grdwhl@gmail.com>
2015-06-06 17:26:46 +0200

    NEW:
    - If a user leavs, all other will see an alert.

--- message.go
+++ message.go
@@ -13,6 +13,7 @@ func NewMessageUserData(userData map[string]interface{}) *message {
 	if name, ok := userData["name"].(string); ok {
 		msg.Message = "Code001 " + name
 		msg.Name = "server.socket"
+		msg.Time = time.Now()
 		return msg
 	}
 	return nil
